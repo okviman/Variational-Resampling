6f87b56cd507f8781a6e286d8df33eee56c4bfaa
okviman <45819512+okviman@users.noreply.github.com>
2023-08-08 13:20:39 +0200

    Update resampling.py

--- resampling.py
+++ resampling.py
@@ -71,6 +71,80 @@ def kl(log_joint, return_multiplicities=False):
         return newAncestors
 
 
+def greedy_alg_beta(log_p, beta=1.0):
+    N = len(log_p)
+    # inputted, unsorted log-likelihood
+
+    # init. multiplicity, particle indices, and f_1/f_0
+    B_s = np.zeros(N, dtype=np.int32)
+    ss = np.arange(0, N)
+    f_add = (B_s + 1) * (log_p - beta * np.log(B_s + 1))
+    f_same = np.zeros_like(log_p)
+
+    # populate heap
+    heap = []
+    for c, idx in zip(f_add, ss):
+        heapq.heappush(heap, (-c, idx))
+    heap = heapsort(heap)
+
+    # distribute multiplicity
+    while np.sum(B_s) < N:
+        C_add_max, t_add = heapq.heappop(heap)
+        f_same[t_add] = f_add[t_add]
+        B_s[t_add] += 1
+        f_add[t_add] = (B_s[t_add] + 1) * (log_p[t_add] - beta * np.log(B_s[t_add] + 1))
+        C_add_new = f_add[t_add] - f_same[t_add]
+        heapq.heappush(heap, (-C_add_new, t_add))
+    newAncestors = np.zeros(N, dtype=np.int32)
+    idx = np.arange(N)
+    for s, b_s in enumerate(B_s):
+        newAncestors[idx[:b_s]] = s
+        idx = idx[b_s:]
+
+    return newAncestors
+
+
+def heapsort(heap):
+    return [heapq.heappop(heap) for _ in range(len(heap))]
+
+
+def cubo(log_joint, return_multiplicities=False):
+    N = len(log_joint)
+    # inputted, unsorted log-likelihood
+    logL_s = 2 * log_joint
+
+    # init. multiplicity, particle indices, and f_1, f_0
+    B_s = np.zeros(N, dtype=np.int32)
+    ss = np.arange(0, N)
+    f_add = (B_s + 1) * np.exp(2 * np.log(B_s + 1) - 2 * logL_s)
+    f_same = np.zeros_like(logL_s)
+
+    # populate heap (negate f_add in order to create max heap)
+    heap = []
+    for c, idx in zip(f_add, ss):
+        heapq.heappush(heap, (-c, idx))
+    heap = heapsort(heap)
+
+    # distribute multiplicity
+    while np.sum(B_s) < N:
+        C_add_max, t_add = heapq.heappop(heap)
+        f_same[t_add] = f_add[t_add]
+        B_s[t_add] += 1
+        f_add[t_add] = (B_s[t_add] + 1) * np.exp(2 * np.log(B_s[t_add] + 1) - 2 * logL_s[t_add])
+        C_add_new = f_add[t_add] - f_same[t_add]
+        heapq.heappush(heap, (-C_add_new, t_add))
+    newAncestors = np.zeros(N, dtype=np.int32)
+    idx = np.arange(N)
+    for s, b_s in enumerate(B_s):
+        newAncestors[idx[:b_s]] = s
+        idx = idx[b_s:]
+
+    if return_multiplicities:
+        return newAncestors, B_s
+    else:
+        return newAncestors
+
+
 def heapsort(heap):
     return [heapq.heappop(heap) for _ in range(len(heap))]
 
